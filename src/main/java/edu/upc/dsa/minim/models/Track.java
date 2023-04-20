eb6d6b4aa4b64eaa19a99170e91b3e2a807bc0bf
nilvelasquez <91841652+nilvelasquez@users.noreply.github.com>
2023-04-20 14:41:17 +0200

    First Track

---
+++ src/main/java/edu/upc/dsa/minim/models/Track.java
@@ -0,0 +1,63 @@
+package edu.upc.dsa.minim.models;
+
+import edu.upc.dsa.util.RandomUtils;
+
+public class Track {
+
+    String id;
+    String title;
+    String singer;
+
+    String  album;
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    static int lastId;
+
+    public Track() {
+        this.id = RandomUtils.getId();
+    }
+
+    public Track(String title, String singer) {
+        this();
+        this.setSinger(singer);
+        this.setTitle(title);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id=id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    @Override
+    public String toString() {
+        return "Track [id="+id+", title=" + title + ", singer=" + singer +"]";
+    }
+
+}
